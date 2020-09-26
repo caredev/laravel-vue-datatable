0ca475d378f9393990937b8a8d5c8b016fb02972
Arkaitz <arkaitz.felices@beautycare.es>
2020-09-26 21:22:43 +0200

    initial dev commit

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,19 @@
-const path = require("path");
+const path = require("path")
 
 module.exports = {
     publicPath: `/laravel-vue-datatable`,
-    outputDir: path.resolve(__dirname, "./docs"),
-    chainWebpack: config => {
-        config.module.rule('md')
-          .test(/\.md/)
-          .use('vue-loader')
-          .loader('vue-loader')
-          .end()
-          .use('vue-markdown-loader')
-          .loader('vue-markdown-loader/lib/markdown-compiler')
-          .options({
-            raw: true
-        })
+    outputDir: path.resolve(__dirname, "./dist"),
+    chainWebpack: (config) => {
+        config.module
+            .rule("md")
+            .test(/\.md/)
+            .use("vue-loader")
+            .loader("vue-loader")
+            .end()
+            .use("vue-markdown-loader")
+            .loader("vue-markdown-loader/lib/markdown-compiler")
+            .options({
+                raw: true,
+            })
     },
 }
